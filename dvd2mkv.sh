faa472ba997c25f34d8ab4ed9e81ee4086ec1f5e
Antonio Kamiya <antonio.kamiya@gmail.com>
2020-06-08 01:56:20 +0900

    use getopts for handling command line. add -c option to ensure conversion doesn't happen automatically. add -j option to translate title from Japanese to English

--- dvd2mkv.sh
+++ dvd2mkv.sh
@@ -1,50 +1,113 @@
 #!/bin/sh
 
-# determine source
-destdir="/Volumes/LHD-ENU3W/Videos/$1"
-if [ -e "$destdir/$1.iso" ]; then
-    src="$destdir/$1.iso"
-else
-    src=$(drutil status |grep -m1 -o '/dev/disk[0-9]*')
-fi
+title=""
+audio_track=""
+subtitle_track=""
+eject=""
+notice=""
+info=""
+translate_japanese=""
+convert=""
 
-if [ -z $src ]; then
-    echo "no source found!"
-    exit
-fi
-
-echo "Using $src as source"
-
-# build command
-eject=0
-notice=0
-cmd=""
-
-if [ -z "$1" ]; then
-    cmd=" --scan"
-    notice=1
-else
-    case "$1" in 
-    eject)
-        eject=1
+while getopts icet:a:s:j opt
+do
+    case $opt in 
+    i)
+        info="1"
+        echo "info selected."
         ;;
-    *)
-        dest="$destdir/$1.mkv"
-        flags=""
-
-        if [ ! -z "$2" ]; then
-            flags="--subtitle \"$2\" --subtitle-burned"
-        fi
-
-        if [ ! -z "$3" ]; then
-            flags="$flags --audio \"$3\""
-        fi
-
-        cmd="-o \"$dest\"  --main-feature  --preset \"Fast 1080p30\" $flags"
-        mkdir "$destdir"
-        eject=1
+    c)
+        convert="1"
+        echo "convert selected."
+        ;;
+ 
+    e)
+        eject="1"
+        echo "eject selected."
+        ;;
+    a)
+        audio_track="$OPTARG"
+        echo "audio_track $OPTARG selected."
+        ;;
+    s)
+        subtitle_track="$OPTARG"
+        echo "subtitle_track $OPTARG selected."
+        ;;
+    t)
+        title="$OPTARG"
+        echo "title=$OPTARG selected."
+        ;;
+    j)
+        translate_japanese="1"
+        echo "translate_japanese selected."
         ;;
     esac
+done
+
+shift $((OPTIND - 1))
+
+# translate title if requested
+if [ ! -z "$translate_japanese" -a ! -z "$title" ]; then
+    jtitle=$(./get_title $title)
+
+    if [ -z "$jtitle" ]; then
+        echo "Title is ambiguous"
+        exit
+    fi
+
+    title=$jtitle
+fi
+
+# determine source and destination
+src=""
+destdir=""
+if [ ! -z "$title" ]; then
+    # determine source
+    destdir="/Volumes/LHD-ENU3W/Videos/$title"
+    if [ -e "$destdir/$title.iso" ]; then
+        src="$destdir/$title.iso"
+    fi
+fi
+
+if [ -z "$src" ]; then
+    src=$(drutil status |grep -m1 -o '/dev/disk[0-9]*')
+
+    if [ -z $src ]; then
+        echo "no source found!"
+    else
+        echo "Using $src as source"
+    fi
+fi
+
+# build command
+cmd=""
+
+if [ ! -z "$src" -a ! -z "$title" -a "$convert" == "1" ]; then
+    dest="$destdir/$title.mkv"
+    flags=""
+
+    if [ ! -z "$subtitle_track" ]; then
+        flags="--subtitle \"$subtitle_track\" --subtitle-burned"
+    fi
+
+    if [ ! -z "$audio_track" ]; then
+        flags="$flags --audio \"$audio_track\""
+    fi
+
+    cmd="-o \"$dest\"  --main-feature  --preset \"Fast 1080p30\" $flags"
+    mkdir -p "$destdir" 
+    eject="1"
+fi
+
+if [ -z "$cmd" -a "$info" == "1" ]; then
+    cmd=" --scan"
+    notice="1"
+fi
+
+echo $eject
+
+if [ -z "$cmd" -a "$eject" != "1" ]; then
+    notice="1"
 fi
 
 # execute ops
@@ -58,5 +121,5 @@ if [ "$eject" == "1" ]; then
 fi
 
 if [ "$notice" == "1" ]; then
-    echo "dvd2mkv [title name (escaped)] [subtitle id] [audio id]"
+    echo "dvd2mkv [-t <title name (escaped)>] [-s <subtitle id>] [-a <audio id>] [-e] [-j]"
 fi
